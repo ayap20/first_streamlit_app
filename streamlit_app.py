bccaf033f960ac53085ba37fcb85fe8007f6ed0a
ayap20 <89419819+ayap20@users.noreply.github.com>
2023-02-27 14:30:21 +0000

    Update streamlit_app.py
    added json formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,13 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display fruityvice fruit advice
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+
+# normalizes json response 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# outputs response as dataframe
+streamlit.dataframe(fruityvice_normalized)
